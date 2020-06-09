dd07879b1492a0c5928125846e75080127a0733a
philipp <s0554213@htw-berlin.de>
2020-06-09 11:43:45 +0200

    separated each sensor in own file

--- sensors/separate.py
+++ sensors/separate.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
 import sys
+import os
 import matplotlib.pyplot as plt
 import csv
 
 data = {}
 keys = []
+directory = "sample_model/"
 
-if len(sys.argv) < 4:
-    print("Arguments must be: 'input file' 'output file' 'start date timestamp (unix)' '*show plot (true | false)' ")
+sys.argv.append("raw/sensordata_oct_2019.csv")
+sys.argv.append("1569888000")
+sys.argv.append("true")
+
+if len(sys.argv) < 3:
+    print("Arguments must be: 'input file' 'start date timestamp (unix)' '*show plot (true | false)' ")
     sys.exit()
 
 input_file = sys.argv[1]
-output_file = sys.argv[2]
-start_date = int(sys.argv[3])
+start_date = int(sys.argv[2])
 
-if len(sys.argv) == 5:
-  show_plot = sys.argv[4].lower() == "true"
+if len(sys.argv) == 4:
+  show_plot = sys.argv[3].lower() == "true"
 else:
   show_plot = False
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 with open(input_file) as f:
   last = start_date - 1200 # minus 20 min
@@ -41,29 +49,30 @@ with open(input_file) as f:
 
           last = time
 
+plot_keys = []
+plot_keys.append(keys[1])
+plot_keys.append(keys[2])
+plot_keys.append(keys[3])
+plot_keys.append(keys[5])
+plot_keys.append(keys[6])
+plot_keys.append(keys[7])
+plot_keys.append(keys[8])
+plot_keys.append(keys[9])
+
+for key in plot_keys:
+  plt.plot(data[keys[0]], data[key], label=key)
+  with open(directory + key.split('[')[0].replace(' ', '_').lower() + ".csv", "w") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(['date', 'value'])
+    writer.writerows(zip(*[data[keys[0]], data[key]]))
+    print("file {0} generated".format(outfile.name))
+
+
+plt.legend(loc="upper left")
+
+plt.grid(True)
+
+data.pop(keys[4])
+
 if show_plot:
-  plot_keys = []
-  plot_keys.append(keys[1])
-  plot_keys.append(keys[2])
-  plot_keys.append(keys[3])
-  plot_keys.append(keys[5])
-  plot_keys.append(keys[6])
-  plot_keys.append(keys[8])
-  plot_keys.append(keys[9])
-
-  for key in plot_keys:
-    plt.plot(data[keys[0]], data[key], label=key)
-
-  plt.legend(loc="upper left")
-
-  plt.grid(True)
-
-data.pop("VL-RL Diff [K]")
-
-with open(output_file, "w") as outfile:
-   writer = csv.writer(outfile)
-   writer.writerow(data.keys())
-   writer.writerows(zip(*data.values()))
-   print("file {0} generated".format(output_file))
-
-plt.show()
+  plt.show()
